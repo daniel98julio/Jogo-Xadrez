c573a7a550a3e2ca9d54da8c1c2125c3f9237546
Daniel Júlio <daniel98julio@gmail.com>
2020-01-14 21:42:14 -0300

    Começando a Implementar um Método que Imprime o Tabuleiro

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -5,10 +5,10 @@
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab) {
+        public Peca(Posicao posicao, Tabuleiro tab, Cor cor) {
             this.posicao = posicao;
-            this.cor = cor;
             this.tab = tab;
+            this.cor = cor;
             this.qteMovimentos = 0;
         }
     }
